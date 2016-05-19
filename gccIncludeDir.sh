bddaa27bf22972ce906f860b7267b12d69fce5b2
Dmitry Ulyanov <sillent1987@gmail.com>
2016-05-19 14:57:20 +0300

    add function implementation. add 2 arguments header|clang

--- gccIncludeDir.sh
+++ gccIncludeDir.sh
@@ -3,11 +3,40 @@
 export LANG=C
 
 GCC=`which gcc`
-cd /tmp
-for i in `$GCC -xc . -v 2>&1|grep include|grep -A10 "#include <"|grep -v "#include"`
-do
-    echo -n "\""
-    echo -n "$i"
-    echo -n "\" "
-done
-echo -en "\n"
+if [ -z $GCC ]
+then
+    echo -en "\"/usr/include/\" \"/usr/local/include\"\n"
+    exit 0
+fi
+
+if [ -z $1 ];
+then
+    echo "Usage: $0 clang|header"
+    exit 1
+fi
+#run main function
+
+function gotIn {
+    cd /tmp
+    for i in `$GCC -xc . -v 2>&1|grep include|grep -A10 "#include <"|grep -v "#include"`
+    do
+        # echo -n "\""
+        if [ $1 == "clang" ];
+        then
+            echo -n "\""
+            echo -n "-I$i"
+            echo -n "\" "
+        elif [ $1 == "header" ];
+        then
+            echo -n "\""
+            echo -n "$i"
+            echo -n "\" "
+        else
+            echo "Not header|clang argument passed"
+        fi
+    done
+    echo -en "\n"
+
+}
+
+gotIn $1
